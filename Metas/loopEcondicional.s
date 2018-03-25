0668b796faee288ebbbb467952e548b2ee5b353b
Manuella Vieira <manuellavr7@gmail.com>
2018-03-25 10:55:52 -0300

    .

--- Metas/loopEcondicional.s
+++ Metas/loopEcondicional.s
@@ -1,17 +1,18 @@
 ######################################################
-# Multiplica r3 por r4 utilizando instruções de soma
+# Multiplica dois valores (r3 e r4)
+# utilizando instruções de soma
 ######################################################
 
 movi r3, 5
-movi r4, 12
+movi r4, 14
 movi r5, 0
 
 loop: 
-	bge r5, r4, parar
+	bge r5, r4, fim 	# para o loop quando o valor de r5 se torna igual ao de r4 
 	add r6, r6, r3
-	addi r5, r5, 1
+	addi r5, r5, 1		
 	br loop
 	
-parar: 
+fim: 
 
 
